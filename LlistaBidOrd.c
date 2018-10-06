ccb3f39250519e2366c3f5c7cecb77fdd12c8741
sitoftonic <sitoftonic@gmail.com>
2018-10-06 23:31:19 +0200

    fix: Modified insereixOrdenat and deleted some unused functions
    Modified insereixOrdenat, which was showing some errors. Also deleted
    some unused functions

--- LlistaBidOrd.c
+++ LlistaBidOrd.c
@@ -6,27 +6,28 @@
 
 
 
-void LLISTABIDORD_crea(LlistaBidOrd *l) {
+LlistaBidOrd LLISTABIDORD_crea() {
+    LlistaBidOrd l;
 
-    l->cap = (Node*) malloc (sizeof(Node));
-    if (l->cap == NULL) {
+    l.cap = (Node*) malloc (sizeof(Node));
+    if (l.cap == NULL) {
         printf("Error: Memory reservation failed when created the list\n");
     }
     else {
-        //l->cap->element = NULL;
-        l->ult = (Node*) malloc (sizeof(Node));
-        if (l->ult == NULL) {
+        l.ult = (Node*) malloc (sizeof(Node));
+        if (l.ult == NULL) {
+            free(l.cap);
             printf("Error: Memory reservation failed when created the list\n");
         }
         else {
-            //l->ult->element = NULL;
-            l->pdi = l->ult;
-            l->cap->seg = l->ult;
-            l->ult->ant = l->cap;
-            l->cap->ant = NULL;
-            l->ult->seg = NULL;
+            l.pdi = l.cap;
+            l.cap->seg = l.ult;
+            l.cap->ant = NULL;
+            l.ult->ant = l.cap;
+            l.ult->seg = NULL;
         }
     }
+    return l;
 }
 
 
@@ -34,125 +35,35 @@ int LLISTABIDORD_insereixOrdenat(LlistaBidOrd *l, int element) {
     int trobat = 0, error = 0;
     Node *aux;
 
-    if (!LLISTABIDORD_buida) {
-        LLISTABIDORD_vesInici(l);
-        while (!trobat) {
-            if (l->pdi->seg->element > element) {
+    l->pdi = l->cap->seg;
+    while (!trobat && l->pdi->seg != NULL) {
+        if (!LLISTABIDORD_buida(*l)) {
+            if (l->pdi->num > element) {
                 l->pdi = l->pdi->seg;
             }
             else {
                 trobat = 1;
             }
         }
-        aux = (Node*) malloc (sizeof(Node));
-        if (aux == NULL) {
-            printf("Error: Memory reservation failed when inserted an element into the list\n");
-            error = 1;
-        }
         else {
-            aux->element = element;
-            aux->seg = l->pdi->seg;
-            l->pdi->seg = aux;
-            aux->ant = l->pdi;
-            aux->seg->ant = aux;
+            trobat = 1;
         }
+    }
+    aux = (Node*) malloc (sizeof(Node));
+    if (aux == NULL) {
+        printf("ERROR");
     }
     else {
-        aux = (Node*) malloc (sizeof(Node));
-        if (aux == NULL) {
-            printf("Error: Memory reservation failed when inserted an element into the list\n");
-            error = 1;
-        }
-        else {
-            aux->element = element;
-            aux->seg = l->cap->seg;
-            l->cap->seg = aux;
-            aux->ant = l->cap;
-            aux->seg->ant = aux;
-        }
+        aux->num = element;
+        aux->seg = l->pdi;
+        aux->ant = l->pdi->ant;
+        l->pdi->ant->seg = aux;
+        l->pdi->ant = aux;
     }
+
     return error;
 }
 
-
-int LLISTABIDORD_consultaElement(LlistaBidOrd l, int posicio, int *element) {
-    int comptador = 1, error = 0;
-
-    if (l.cap == l.ult) {
-        printf("Error: Empty list\n");
-    }
-    else {
-        LLISTABIDORD_vesInici(&l);
-        while (comptador != posicio && !error) {
-            if (l.pdi != l.ult) {
-                l.pdi = l.pdi->seg;
-                comptador++;
-            }
-            else {
-                error = 1;
-            }
-        }
-        if (!error) {
-            if (l.pdi->seg == NULL) {
-                error = 1;
-            }
-            else {
-                (*element) = l.pdi->element;
-            }
-        }
-    }
-    return error;
-}
-
-
-int LLISTABIDORD_esborra(LlistaBidOrd *l, int posicio) {
-    Node *aux;
-    int comptador = 0, error = 0;
-
-    if (l->cap == l->ult) {
-        printf("Error: Empty list\n");
-    }
-    else {
-        LLISTABIDORD_vesInici(l);
-        while (comptador != posicio && !error) {
-            if (l->pdi != l->ult) {
-                l->pdi = l->pdi->seg;
-                comptador++;
-            }
-            else {
-                printf("Error: Reached the last item of the list\n");
-                error = 1;
-            }
-        }
-        if (!error) {
-            aux = (Node*) malloc (sizeof(Node));
-            if (aux == NULL) {
-                printf("Error: Memory reservation failed when inserted an element into the list\n");
-            }
-            else {
-                aux = l->pdi;
-                aux->ant->seg = aux->seg;
-                aux->seg->ant = aux->ant;
-                l->pdi = l->pdi->seg;
-                free(aux);
-            }
-        }
-    }
-    return error;
-}
-
-
-void LLISTABIDORD_mostra(LlistaBidOrd l) {
-    int comptador = 1;
-
-    printf("###########################################\n\tPOSICIO\t\tELEMENT\n");
-    LLISTABIDORD_vesInici(&l);
-    while (l.pdi != l.ult) {
-        printf("\t%d\t\t%d\n", comptador, l.pdi->element);
-        comptador++;
-        l.pdi = l.pdi->seg;
-    }
-}
 
 
 void LLISTABIDORD_vesInici(LlistaBidOrd *l) {
@@ -181,3 +92,36 @@ void LLISTABIDORD_destrueix(LlistaBidOrd *l) {
     l->ult = NULL;
     l->pdi = NULL;
 }
+
+int LLISTABIDORD_getSize(LlistaBidOrd l) {
+    int size = 0;
+
+    LLISTABIDORD_vesInici(&l);
+    while (!LLISTABIDORD_fi(l)) {
+        LLISTABIDORD_avanca(&l);
+        size++;
+    }
+    return size;
+}
+
+int LLISTABIDORD_fi(LlistaBidOrd l) {
+    return l.pdi == l.ult;
+}
+
+int LLISTABIDORD_inici(LlistaBidOrd l) {
+    return l.pdi == l.cap;
+}
+
+void LLISTABIDORD_avanca(LlistaBidOrd *l) {
+    l->pdi = l->pdi->seg;
+}
+
+void LLISTABIDORD_retrocedeix(LlistaBidOrd *l) {
+    l->pdi = l->pdi->ant;
+}
+
+int LLISTABIDORD_consulta(LlistaBidOrd l) {
+   return l.pdi->num;
+}
+
+
