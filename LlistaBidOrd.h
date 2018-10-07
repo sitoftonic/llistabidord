3fb4fac793616f8eb9f446e3a272381679c4ef10
sitoftonic <sitoftonic@gmail.com>
2018-10-07 22:38:12 +0200

    doc: Added function declaration comments
    Added short comment for each function declaration in .h file

--- LlistaBidOrd.h
+++ LlistaBidOrd.h
@@ -10,6 +10,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+/* Structure definition of the list */
 
 typedef struct n {
     int num;
@@ -23,17 +24,44 @@ typedef struct {
     Node *pdi;
 } LlistaBidOrd;
 
+
+
+
+
+// Method to create the list
 LlistaBidOrd LLISTABIDORD_crea();
+
+// Method to insert element into the list. Returns error
 int LLISTABIDORD_insereixOrdenat(LlistaBidOrd *l, int element);
+
+// Method to go to the first element of the list
 void LLISTABIDORD_vesInici(LlistaBidOrd *l);
+
+// Method to go to the last element of the list
 void LLISTABIDORD_vesFinal(LlistaBidOrd *l);
+
+// Method to establish if the list is empty. Returns 1 or 0
 int LLISTABIDORD_buida(LlistaBidOrd l);
+
+// Method to destroy every node and the list structure
 void LLISTABIDORD_destrueix(LlistaBidOrd *l);
+
+// Method to get the size of the list
 int LLISTABIDORD_getSize(LlistaBidOrd l);
+
+// Method to establish if pdi is pointing the last element of the list
 int LLISTABIDORD_fi(LlistaBidOrd l);
-void LLISTABIDORD_avanca(LlistaBidOrd *l);
-void LLISTABIDORD_retrocedeix(LlistaBidOrd *l);
+
+// Method to establish if pdi is pointing the first element of the list
 int LLISTABIDORD_inici(LlistaBidOrd l);
+
+// Method to go forward inside the list
+void LLISTABIDORD_avanca(LlistaBidOrd *l);
+
+// Method to go back inside the list
+void LLISTABIDORD_retrocedeix(LlistaBidOrd *l);
+
+// Method to establish the number that is pointed by pdi. Returns the value
 int LLISTABIDORD_consulta(LlistaBidOrd l);
 
 
